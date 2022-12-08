4084b113d0399343c2fd12e87c13ebe81f594009
ismail barış öztürk <ibarisozturk95@outlook.com.tr>
2022-12-08 21:57:36 +0300

    Add project files.

---
+++ Gorevcim.Core/Models/BaseCompany.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorevcim.Core.Models
+{
+    public abstract class BaseCompany
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public int CompanyId { get; set; }
+        public string CompanyAdress { get; set; }
+
+
+
+
+
+
+    }
+}
